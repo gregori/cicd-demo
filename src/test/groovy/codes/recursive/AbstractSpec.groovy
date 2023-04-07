a73f3689c44e05fae83cc8de73e3878d574b3533
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2023-04-07 15:15:06 -0300

    add testing capabilities

---
+++ src/test/groovy/codes/recursive/AbstractSpec.groovy
@@ -0,0 +1,16 @@
+package codes.recursive
+
+import io.micronaut.context.ApplicationContext
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+class AbstractSpec extends Specification {
+    @Shared
+    @AutoCleanup
+    static ApplicationContext context
+
+    static {
+        context = ApplicationContext.run()
+    }
+}
---
+++ src/test/groovy/codes/recursive/HelloWorldSpec.groovy
@@ -0,0 +1,14 @@
+package codes.recursive
+
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+
+@MicronautTest
+class HelloWorldSpec extends AbstractSpec {
+    def "test hello world"() {
+        def foo = 'bar'
+        when:
+        foo == 'bar'
+        then:
+        foo.reverse() == 'rab'
+    }
+}
